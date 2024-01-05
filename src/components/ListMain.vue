c74eb8e44a1ee632f47589195708c631b710bb99
hopeduckworth <hope.duckworth@gmail.com>
2024-01-04 22:57:47 -0500

    fixed styling of main list card

---
+++ src/components/ListMain.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+type ListMainProps = {
+  title: string;
+  items: number;
+};
+
+defineProps<ListMainProps>();
+</script>
+
+<template>
+  <div class="container">
+    <p class="large-text">
+      {{ title }}
+    </p>
+
+    <p class="small-text">{{ items }} items</p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  color: white;
+  margin: 0;
+}
+
+p.small-text {
+  font-size: 16px;
+}
+
+p.large-text {
+  font-size: 20px;
+  margin-bottom: 0.75rem;
+}
+
+.container {
+  background-color: black;
+  padding: 1.5rem 2rem;
+  border-radius: 16px;
+  flex-direction: column;
+  display: flex;
+  align-items: flex-start;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="This is a test message" />
+    <!-- <HelloWorld msg="This is a test message" /> -->
+    <ListMain title="Test Title" :items="5" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import ListMain from "@/components/ListMain.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    ListMain,
   },
 });
 </script>
