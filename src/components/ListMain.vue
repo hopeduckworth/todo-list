6ad10a0d89b5daa4454e4a15a9d897fb3d31223f
hopeduckworth <hope.duckworth@gmail.com>
2024-01-05 22:19:35 -0500

    finished main list card

--- src/components/ListMain.vue
+++ src/components/ListMain.vue
@@ -2,7 +2,9 @@
 import { defineProps } from "vue";
 
 type ListMainProps = {
+  /** this is the title of the to do list */
   title: string;
+  /** this is the number of items in the to do list */
   items: number;
 };
 
@@ -23,23 +25,30 @@ defineProps<ListMainProps>();
 p {
   color: white;
   margin: 0;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+  font-family: Poppins;
 }
 
 p.small-text {
-  font-size: 16px;
+  font-size: 1rem;
 }
 
 p.large-text {
-  font-size: 20px;
   margin-bottom: 0.75rem;
+  font-size: 1.25rem;
 }
 
 .container {
   background-color: black;
   padding: 1.5rem 2rem;
-  border-radius: 16px;
   flex-direction: column;
   display: flex;
   align-items: flex-start;
+  max-width: 600px;
+  border-radius: 0.5rem;
+  border: 1px solid #fff;
 }
 </style>
