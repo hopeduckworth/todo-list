c1b61f171553a87098711ad9b6aa4548a7952a49
hopeduckworth <hope.duckworth@gmail.com>
2024-01-05 22:50:34 -0500

    added list item card

---
+++ src/components/ListItem.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+type ListItemProps = {
+  /** this is the title of the to do list */
+  title: string;
+  /** this is an optional subtitle for the card */
+  subtitle?: string;
+};
+
+defineProps<ListItemProps>();
+</script>
+
+<template>
+  <div class="container">
+    <p class="large-text">
+      {{ title }}
+    </p>
+
+    <p v-if="subtitle" class="small-text">{{ subtitle }}</p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  color: white;
+  margin: 0;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+  font-family: Poppins;
+}
+
+p.small-text {
+  font-size: 1rem;
+}
+
+p.large-text {
+  margin-bottom: 0.75rem;
+  font-size: 1.25rem;
+}
+
+.container {
+  background-color: black;
+  padding: 1.5rem 2rem;
+  flex-direction: column;
+  display: flex;
+  align-items: flex-start;
+  max-width: 600px;
+  border-radius: 0.5rem;
+  border: 1px solid #fff;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <!-- <HelloWorld msg="This is a test message" /> -->
     <ListMain title="Test Title" :items="5" />
+    <ListItem title="Test List" subtitle="test subtitle" />
+    <ListItem title="Test List" />
   </div>
 </template>
 
@@ -10,12 +12,14 @@
 import { defineComponent } from "vue";
 // import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import ListMain from "@/components/ListMain.vue";
+import ListItem from "@/components/ListItem.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     // HelloWorld,
     ListMain,
+    ListItem,
   },
 });
 </script>
